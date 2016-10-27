1803e148afdc7ef23414c86d134da16ccb9f956e
jiayuanbin <jiayuanbin@supersuer.com>
2016-10-27 18:45:09 +0800

    add functions

--- app/src/main/java/com/sunnyxibei/forktest/MainActivity.java
+++ app/src/main/java/com/sunnyxibei/forktest/MainActivity.java
@@ -18,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String stringFromJni = getStringFromJni();
         if (null != stringFromJni) {
-            TastyToast.makeText(this,stringFromJni,TastyToast.LENGTH_SHORT,TastyToast.DEFAULT);
-            TastyToast.makeText(this,"交叉编译出的.so动态链接库已经成功运行！",TastyToast.LENGTH_LONG,TastyToast.SUCCESS);
+            TastyToast.makeText(this, stringFromJni, TastyToast.LENGTH_SHORT, TastyToast.DEFAULT);
+            TastyToast.makeText(this, "交叉编译出的.so动态链接库已经成功运行！", TastyToast.LENGTH_LONG, TastyToast.SUCCESS);
         }
         getFork();
     }
 
-    public void openBrowser(View view){
+    public void openBrowser(View view) {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -38,4 +38,6 @@ public class MainActivity extends AppCompatActivity {
     public native String getStringFromJni();
 
     public native void testExeclp();
+
+    public static native void patch(String oldApkPath, String newApkPath, String patchPath);
 }
--- app/src/main/jni/mainactivity.c
+++ app/src/main/jni/mainactivity.c
@@ -3,9 +3,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "bspatch.h"
+
+// 获取数组的大小
+# define NELEM(x) ((int) (sizeof(x) / sizeof((x)[0])))
+
+// 指定要注册的类，对应完整的java类名
+//#define JNIREG_CLASS "com/chan/ypatchcore/YPatch"
+#define JNIREG_CLASS "com/sunnyxibei/forktest/MainActivity"
+
 
 #define LOG_TAG "System.out"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+
+JNIEXPORT void JNICALL
+Java_com_sunnyxibei_forktest_MainActivity_patch
+        (JNIEnv *env, jclass clazz, jstring oldFile, jstring newFile, jstring patchFile) {
+    int args = 4;
+    char *argv[args];
+    argv[0] = "bspatch";
+    argv[1] = (char *) ((*env)->GetStringUTFChars(env, oldFile, 0));
+    argv[2] = (char *) ((*env)->GetStringUTFChars(env, newFile, 0));
+    argv[3] = (char *) ((*env)->GetStringUTFChars(env, patchFile, 0));
+
+    //此处executePathch()就是上面我们修改出的
+    run_patch(oldFile, newFile, patchFile);
+
+    (*env)->ReleaseStringUTFChars(env, oldFile, argv[1]);
+    (*env)->ReleaseStringUTFChars(env, newFile, argv[2]);
+    (*env)->ReleaseStringUTFChars(env, patchFile, argv[3]);
+}
 
 JNIEXPORT void JNICALL
 Java_com_sunnyxibei_forktest_MainActivity_getFork(JNIEnv *env, jobject instance) {
@@ -51,7 +78,6 @@ Java_com_sunnyxibei_forktest_MainActivity_getFork(JNIEnv *env, jobject instance)
 
 JNIEXPORT jstring JNICALL
 Java_com_sunnyxibei_forktest_MainActivity_getStringFromJni(JNIEnv *env, jobject instance) {
-
     return (*env)->NewStringUTF(env, "Hello from JNI !");
 }
 
@@ -61,4 +87,39 @@ Java_com_sunnyxibei_forktest_MainActivity_testExeclp(JNIEnv *env, jobject instan
     execlp("am", "am", "start", "--user", "0", "-a",
            "android.intent.action.VIEW", "-d",
            "https://sunnyxibei.github.io/", (char *) NULL);
+}
+
+// Java和JNI函数的绑定表
+static JNINativeMethod method_table[] = {
+        {"patch", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V",
+                (void *) Java_com_sunnyxibei_forktest_MainActivity_patch}
+};
+
+// 注册native方法到java中
+static int registerNativeMethods(JNIEnv *env, const char *className, JNINativeMethod *gMethods,
+                                 int numMethods) {
+    jclass clazz;
+    clazz = (*env)->FindClass(env, className);
+    if (clazz == NULL) {
+        return JNI_FALSE;
+    }
+    if ((*env)->RegisterNatives(env, clazz, gMethods, numMethods) < 0) {
+        return JNI_FALSE;
+    }
+    return JNI_TRUE;
+}
+
+int register_ndk_load(
+        JNIEnv *env) {    // 调用注册方法
+    return registerNativeMethods(env, JNIREG_CLASS, method_table, NELEM(method_table));
+}
+
+JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+    JNIEnv *env = NULL;
+    jint result = -1;
+    if ((*vm)->GetEnv(vm, (void **) &env, JNI_VERSION_1_4) != JNI_OK) {
+        return result;
+    }
+    register_ndk_load(env);
+    return JNI_VERSION_1_4;
 }
